8df0043bc6bcb6753b90d1e1c49e1978d5ab7d33
sasasd96 <stroketi@gmail.com>
2025-09-23 15:30:53 +0200

    Add files via upload

--- script/c20.lua
+++ script/c20.lua
@@ -12,62 +12,67 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--ATK/DEF based on banished cards
+	--Unaffected by opponent's card effects while Macro Cosmos is on field
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_SET_ATTACK)
-	e2:SetValue(s.atkval)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetCondition(s.immcon)
+	e2:SetValue(s.efilter)
 	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_SET_DEFENSE)
+	--ATK/DEF based on banished cards
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_SET_ATTACK)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--Multiple attacks
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
-	e4:SetValue(2)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_SET_DEFENSE)
 	c:RegisterEffect(e4)
-	--Banish opponent's deck on summon
+	--Multiple attacks
 	local e5=Effect.CreateEffect(c)
-	e5:SetCategory(CATEGORY_REMOVE)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e5:SetTarget(s.rmtg)
-	e5:SetOperation(s.rmop)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e5:SetValue(2)
 	c:RegisterEffect(e5)
+	--Banish opponent's deck on summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_REMOVE)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e6:SetTarget(s.rmtg)
+	e6:SetOperation(s.rmop)
+	c:RegisterEffect(e6)
 end
+s.listed_names={54493213,80887952,17286057,30241314} --Helios monsters + Macro Cosmos
 
 function s.spfilter(c)
-	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:GetAttack()==c:GetDefense() and c:IsAbleToDeck()
+	return (c:IsCode(54493213) or c:IsCode(80887952) or c:IsCode(17286057)) and c:IsAbleToDeck()
 end
 
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD+LOCATION_REMOVED,0,nil)
-	return g:GetClassCount(Card.GetCode)>=3
+	return g:IsExists(Card.IsCode,1,nil,54493213) and g:IsExists(Card.IsCode,1,nil,80887952) and g:IsExists(Card.IsCode,1,nil,17286057)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
 	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD+LOCATION_REMOVED,0,nil)
 	local sg=Group.CreateGroup()
-	local codes={}
-	local tc=g:GetFirst()
-	while tc do
-		local code=tc:GetCode()
-		if not codes[code] then
-			codes[code]=true
-			sg:AddCard(tc)
-		end
-		tc=g:GetNext()
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local tg=sg:Select(tp,3,3,nil)
-	if tg then
-		tg:KeepAlive()
-		e:SetLabelObject(tg)
+	--Select one of each required Helios monster
+	local helios1=g:Filter(Card.IsCode,nil,54493213):GetFirst()
+	local helios2=g:Filter(Card.IsCode,nil,80887952):GetFirst()
+	local helios3=g:Filter(Card.IsCode,nil,17286057):GetFirst()
+	if helios1 then sg:AddCard(helios1) end
+	if helios2 then sg:AddCard(helios2) end
+	if helios3 then sg:AddCard(helios3) end
+	if #sg==3 then
+		sg:KeepAlive()
+		e:SetLabelObject(sg)
 		return true
 	else
 		return false
@@ -80,6 +85,18 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
 		g:DeleteGroup()
 	end
+end
+
+function s.immcon(e)
+	return Duel.IsExistingMatchingCard(s.macrofilter,e:GetHandlerPlayer(),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+end
+
+function s.macrofilter(c)
+	return c:IsFaceup() and c:IsCode(30241314)
+end
+
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
 end
 
 function s.atkval(e,c)
