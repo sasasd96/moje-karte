4fa47c41619341aff3d50b6fc72a6ed95fff72fc
sasasd96 <stroketi@gmail.com>
2025-09-26 16:53:33 +0200

    Delete script directory

--- script/c225.lua
+++
@@ -1,6 +0,0 @@
---Orichalcos Stratiotis
-local s,id=GetID()
-function s.initial_effect(c)
-	--No effect (vanilla monster)
-end
-s.listed_series={0x12ab}
--- script/c226.lua
+++
@@ -1,6 +0,0 @@
---Orichalcos Soldier
-local s,id=GetID()
-function s.initial_effect(c)
-	--No effect (vanilla monster)
-end
-s.listed_series={0x12ab}
