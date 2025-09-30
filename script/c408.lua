72351ebd2af110a1c99c621af946ca752f36b59e
sasasd96 <stroketi@gmail.com>
2025-09-30 17:32:55 +0200

    Add files via upload

--- script/c408.lua
+++ script/c408.lua
@@ -93,11 +93,11 @@ function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	--Gain 2500 ATK
+	--Gain 1000 ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(2500)
+	e1:SetValue(1000)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 	c:RegisterEffect(e1)
 end
